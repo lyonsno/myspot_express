c67ef783b4d5b0f7c6515b972827d8f62a1ac580
Noah Lyons <noahlyons@Noahs-MacBook-Pro.local>
2017-06-07 17:40:57 -0700

    post form submitting body properly

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,65 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('knex')(require('../knexfile'))
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', message: 'Yo Yo' });
+router.get('/', function(req, res) {
+	res.render('index', { title: 'Express', message: 'Yo Yo' });
 });
+
+router.post('/api/spots', (req, res) => {
+	var spotName = req.body.spotName;
+	return res.send(req.body);
+	// res.send(JSON.stringify(req.body));	
+	// res.send(spotName);
+	knex('spot')
+	.returning('id')
+	.insert({
+		name: spotName
+	})
+	.then(function(params){
+		res.sendStatus(200);
+	});
+});
+
+router.get('/api/spots', (req, res) => {
+	res.contentType('application/json');
+	var params = knex.select('*').from('spot').then(function(params){
+		res.send(params[0]);
+	});
+});
+
+//access spot by id
+router.get('/api/spots/:spotId/', (req, res) => {
+	var spotId = req.params.spotId
+	knex.select('*').from('spot').where('id', spotId)
+	.then(function(values){
+		res.send(values);
+	});
+});
+
+//access spot page by spot name
+router.get('/spots/:spotName/', (req, res) => {
+	var spotName = req.params.spotName
+	knex.select('*').from('spot').where('name', spotName).then(function(values){
+	res.render('spot', { id: "spot"})
+	});
+});
+
+router.get('/spotsjson', (req, res) => {
+	var spots = knex.select('*').from('spot').then(function(spots){
+		res.json(spots);
+	});
+});
+
+router.get('/monkey/bear/:potato/:robots', function (req, res) {
+	var thing = `${req.params.potato} ${req.params.robots}`
+	res.render(
+		'index',
+		{
+			title: thing,
+			message: thing
+		})
+})
 
 module.exports = router;
