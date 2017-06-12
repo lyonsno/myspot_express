56003f28fe15327242986ae4fa11d1c9910264e9
Noah Lyons <noahlyons@Noahs-MacBook-Pro.local>
2017-06-11 17:52:25 -0700

    clean up

--- routes/index.js
+++ routes/index.js
@@ -3,82 +3,91 @@ var router = express.Router();
 var knex = require('knex')(require('../knexfile'))
 
 /* GET home page. */
-router.get('/', function(req, res) {
-	res.render('index', { title: 'Express', message: 'Yo Yo' });
-});
-
-router.post('/api/spots', (req, res) => {
-	res.contentType('application/json');
-	var spotName = req.body.newSpotName;
-	knex('spot')
-	.returning()
-	.insert({
-		name: spotName
-	})
-	.then(function(params){
-		res.send(params).status(200);
+{
+	router.get('/', function(req, res) {
+		res.render('index', { title: 'Express', message: 'Yo Yo' });
 	});
-});
+}
 
-router.get('/api/spots', (req, res) => {
-	res.contentType('application/json');
-	var params = knex.select('*').from('spot').then(function(params){
-		res.status(200).send({ 'data': params });
+/* spots */
+{
+	// create a new spot
+	router.post('/api/spots', (req, res) => {
+		res.contentType('application/json');
+		var spotName = req.body.newSpotName;
+		knex('spot')
+		.returning()
+		.insert({
+			name: spotName
+		})
+		.then(function(params){
+			res.send(params).status(200);
+		});
 	});
-});
 
-//access spot by id
-router.get('/api/spots/:spotId/', (req, res) => {
-	var spotId = req.params.spotId
-	knex.select('*').from('spot').where('id', spotId)
-	.then(function(values){
-		res.status(200).send(values);
+	//get all spots
+	router.get('/api/spots', (req, res) => {
+		res.contentType('application/json');
+		var params = knex.select('*').from('spot').then(function(params){
+			res.status(200).send({ 'data': params });
+		});
 	});
-});	
 
-router.get('/spots', (req, res) => {
-	knex.select('*').from('spot').then(function(values)
-	{
-		res.render('spots',
+	//get spot by id
+	router.get('/api/spots/:spotId/', (req, res) => {
+		var spotId = req.params.spotId
+		knex.select('*').from('spot').where('id', spotId)
+		.then(function(values){
+			res.status(200).send(values);
+		});
+	});	
+
+	//view page of all spots
+	router.get('/spots', (req, res) => {
+		knex.select('*').from('spot').then(function(values)
 		{
-			allSpots: values
+			res.render('spots',
+			{
+				allSpots: values
+			});
 		});
 	});
-});
 
-//access spot page by spot name
-router.get('/spots/:spotName/', (req, res) => {
-	var spotName = req.params.spotName
-	knex.select('*').from('spot').where('name', spotName).then(function(values){
-	// res.send(values);
-		res.render('spot', 
-		{ 
-			name: spotName,
-			id: values[0].id
+	//access spot page by spot name
+	router.get('/spots/:spotName/', (req, res) => {
+		var spotName = req.params.spotName
+		knex.select('*').from('spot').where('name', spotName).then(function(values){
+		// res.send(values);
+			res.render('spot', 
+			{ 
+				name: spotName,
+				id: values[0].id
+			});
 		});
 	});
-});
+}
 
-
-router.post('/api/lists', (req, res) => {
-	var listName = req.body.newListName;
-	knex('list')
-	.returning('id')
-	.insert({
-		name: listName
-	})
-	.then(function(params){
-		res.send(req.body);
-		res.sendStatus(200);
+/* lists */
+{
+	router.post('/api/lists', (req, res) => {
+		var listName = req.body.newListName;
+		knex('list')
+		.returning('id')
+		.insert({
+			name: listName
+		})
+		.then(function(params){
+			res.send(req.body);
+			res.sendStatus(200);
+		});
 	});
-});
 
-router.get('/api/lists', (req, res) => {
-	res.contentType('application/json');
-	knex.select('*').from('list').then(function(params){
-		res.send(params[0]);
+	router.get('/api/lists', (req, res) => {
+		res.contentType('application/json');
+		knex.select('*').from('list').then(function(params){
+			res.send(params[0]);
+		});
 	});
-});
-
+}
 
 module.exports = router;
