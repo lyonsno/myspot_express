4cb2f54213822fc439323f850a15b984b05bbdf8
Noah Lyons <noahlyons@Noahs-MacBook-Pro.local>
2017-06-19 19:42:22 -0700

    should add new entry to database passing

--- routes/lists.js
+++ routes/lists.js
@@ -49,7 +49,6 @@ var knex = require('knex')(require('../knexfile'))
 		console.log("about to post entry\n spotId: " + req.body.spotId);
 		var listId = req.params.id;
 		var spotId = req.body.spotId;
-		res.sendStatus(200);
 		knex('entry')
 		.returning()
 		.insert({
